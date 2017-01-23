d8f64f6ec397b53e669cbd2c082f5106d125dd17
Eric Hong <erichongalldaylong@gmail.com>
2017-01-22 17:11:30 -0800

    database stuff

--- CultureMesh/dbtest.py
+++ CultureMesh/dbtest.py
@@ -1,24 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 from flaskext.mysql import MySQL
- 
-mysql = MySQL()
 app = Flask(__name__)
+mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'culturp7_eric'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'mysqlhm'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'culturemesh1'
 app.config['MYSQL_DATABASE_DB'] = 'culturp7_ktc'
 app.config['MYSQL_DATABASE_HOST'] = '50.116.65.175'
 mysql.init_app(app)
- 
+
 @app.route("/")
-def hello():
+def home():
 	cursor = mysql.connect().cursor()
 	cursor.execute("SHOW TABLES")
 	tablenames = cursor.fetchall()
-	print type(tablenames)
-	print tablenames
-	return "Welcome to Python Flask App!"
+	return str(tablenames)
 
 if __name__ == "__main__":
-    app.run()
-
-# WHOOOOO :)
+	app.run()
--- CultureMesh/run.py
+++ CultureMesh/run.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template, request
-app = Flask(__name__)
-
 from flaskext.mysql import MySQL 
-mysql = MySQL()
 app = Flask(__name__)
+mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'culturp7_eric'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'mysqlhm'
-app.config['MYSQL_DATABASE_DB'] = 'culturp7_ktc'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'culturemesh17'
+app.config['MYSQL_DATABASE_DB'] = 'culturp7_rehearsal'
 app.config['MYSQL_DATABASE_HOST'] = '50.116.65.175'
 mysql.init_app(app)
 
 visitors = 0
+
+# from dbroutes import db_api
+# app.register_blueprint(db_api)
 
 @app.route("/count")
 def hello():
@@ -22,9 +23,8 @@ def hello():
 def about():
 	return "<img src='https://s30.postimg.org/958ms8rip/Screen_Shot_2016_12_07_at_5_46_23_PM.png' height='500px'></img>dwefoie</title><b>HAHHAHHHH</b>"
 
-@app.route("/syriatosanfrancisco")
+@app.route("/showtables")
 def get():
-	#return connecttodatabase("syria sanfrancisco")
 	cursor = mysql.connect().cursor()
 	cursor.execute("SHOW TABLES")
 	tablenames = cursor.fetchall()
@@ -38,7 +38,18 @@ def home():
 def search():
 	from_location = request.args.get("from")
 	in_location = request.args.get("in")
-	return from_location + in_location
+	print 1
+	cursor = mysql.connect().cursor()
+	print 2
+	cursor.execute('select population, region_name, country_name from cities where name="' + from_location + '"')
+	print 3
+	from_info = cursor.fetchall()
+	print 4
+	cursor.execute('select population, region_name, country_name from cities where name="' + in_location + '"')
+	print 5
+	in_info = cursor.fetchall()
+	print 6
+	return from_location + ": " + str(from_info) + "<br>" + in_location + ": " + str(in_info)
 
 @app.route("/retrieve_tables_and_columns")
 def retrieve_tables_and_columns():
@@ -67,6 +78,16 @@ def select_users():
 	for u in users:
 		s += str(u) + "<br \\>"
 	return s
+
+@app.route("/sql")
+def sql():
+	cursor = mysql.connect().cursor()
+	cmd = request.args.get("command")
+	if cmd != None:
+		cursor.execute(cmd) 
+		return str(cursor.fetchall()) + '<br><br><form action="sql"><input type="text" name="command" placeholder="SQL command"></input></form>'
+	else:
+		return '<form action="sql"><input type="text" name="command" placeholder="SQL command"></input></form>'
 
 def retrieve(from_location, in_location):
 	pass
