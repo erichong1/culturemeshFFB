e57f319f9d0e9a087728ec62afc98071671cb1eb
Eric Hong <erichongalldaylong@gmail.com>
2017-02-26 16:23:09 -0800

    diagnostic blueprint

--- CultureMesh/run.py
+++ CultureMesh/run.py
@@ -11,50 +11,17 @@ mysql.init_app(app)
 import hashlib
 
 from network import network
+from dbdiagnostics import dbdiagnostics
 app.register_blueprint(network)
+app.register_blueprint(dbdiagnostics)
 
-# from dbroutes import db_api
-# app.register_blueprint(db_api)
-visitors = 0
-@app.route("/count")
-def hello():
-	global visitors
-	visitors += 1
-	return str(visitors)
+@app.route("/")
+def home():
+	return render_template('index.html')
 
 @app.route("/about")
 def about():
 	return "<img src='https://s30.postimg.org/958ms8rip/Screen_Shot_2016_12_07_at_5_46_23_PM.png' height='500px'></img>dwefoie</title><b>HAHHAHHHH</b>"
-
-@app.route("/showtables")
-def get():
-	cursor = mysql.connect().cursor()
-	cursor.execute("SHOW TABLES")
-	tablenames = cursor.fetchall()
-	return "tablenames: " + str(tablenames)
-
-@app.route("/")
-def home():
-	print "wehfeif"
-	return render_template('index.html')
-
-@app.route("/retrieve_tables_and_columns")
-def retrieve_tables_and_columns():
-	cursor = mysql.connect().cursor()
-	cursor.execute("SHOW TABLES")
-	tablenames = cursor.fetchall()
-	complete_list = ""
-	for i in xrange(len(tablenames)):
-		cur_table = str(tablenames[i]).split("'")[1]
-		complete_list += cur_table
-		complete_list += ": "
-		#cursor = mysql.connect().cursor()
-		cursor.execute("DESCRIBE " + cur_table)
-		cur_table_columns = cursor.fetchall()
-		for j in cur_table_columns:
-			complete_list += str(j[0]) + ", "
-		complete_list += "<br \\><br \\>"
-	return complete_list
 
 @app.route("/select_users")
 def select_users():
@@ -65,16 +32,6 @@ def select_users():
 	for u in users:
 		s += str(u) + "<br \\>"
 	return s
-
-@app.route("/sql")
-def sql():
-	cursor = mysql.connect().cursor()
-	cmd = request.args.get("command")
-	if cmd != None:
-		cursor.execute(cmd) 
-		return str(cursor.fetchall()) + '<br><br><form action="sql"><input type="text" name="command" placeholder="SQL command"></input></form>'
-	else:
-		return '<form action="sql"><input type="text" name="command" placeholder="SQL command"></input></form>'
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
