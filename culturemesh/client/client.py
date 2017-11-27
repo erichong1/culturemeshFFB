d6ce8e0a892ac1634af7f625201468815c235bf4
Alan <alanf94@stanford.edu>
2017-11-26 20:41:11 -0800

    registered API calls with the client object

--- culturemesh/client/client.py
+++ culturemesh/client/client.py
@@ -62,11 +62,44 @@ class Client(object):
 
 
 """ Register the client with the API functions. """
+
 from .example_api_module import get_gutenberg_novel
+from .events import get_event
+from .events import get_event_attendance_list
+from .events import create_event
+from .events import update_event
+from .posts import get_post
+from .posts import get_post_replies
+from .posts import create_post
+from .posts import create_post_reply
+from .users import get_users
 from .users import get_user
- ## Add more API call imports here
+from .users import get_user_networks
+from .users import get_user_posts
+from .users import get_user_events
+from .users import create_user
+from .users import add_user_to_event
+from .users import add_user_to_network
+from .users import update_user
+
+# We may consider adding a wrapper around these assignments
+# below to introduce more specific features for the client.
 
 Client.get_gutenberg_novel = get_gutenberg_novel
+Client.get_event = get_event
+Client.get_event_attendance_list = get_event_attendance_list
+Client.create_event = create_event
+Client.update_event = update_event
+Client.get_post = get_post
+Client.get_post_replies = get_post_replies
+Client.create_post = create_post
+Client.create_post_reply = create_post_reply
+Client.get_users = get_users
 Client.get_user = get_user
- ## Every single method imported above must be registered
- ## with the client here.
+Client.get_user_networks = get_user_networks
+Client.get_user_posts = get_user_posts
+Client.get_user_events = get_user_events
+Client.create_user = create_user
+Client.add_user_to_event = add_user_to_event
+Client.add_user_to_network = add_user_to_network
+Client.update_user = update_user
