d2553a077a439edfa6966fe2d4f12cfb4d8cdb74
Alan <alanf94@stanford.edu>
2017-11-29 23:39:26 -0800

    client exceptions, and mock data to return user-related GET API info, with the exception of networks

--- test/client/test_example_api_module.py
+++ test/client/test_example_api_module.py
@@ -7,19 +7,4 @@ from nose.tools import assert_true
 from culturemesh.client import Client
 import requests
 
-def test_get_dracula():
-	"""
-	Tests we can retrieve Dracula. 
-	"""
-
-	c = Client(mock=False)
-
-	# Here, we want to mock the client. 
-	
-	dracula_ebook_num = 345
-
-	text = c.get_gutenberg_novel(dracula_ebook_num)
-	assert_true('Dracula' in text)
-	assert_true('Bram Stoker' in text)
-
-## More API call methods would go below here..
+# TODO: delete
--- test/client/test_users.py
+++ test/client/test_users.py
@@ -10,9 +10,44 @@ def test_get_user():
 	Tests we can retrieve a user.  For illustrative purposes, the client is not mocked!
 	"""
 
-	c = Client()
+	c = Client(mock=True)
 
 	# Here, we want to mock the client. 
 	
 	user = c.get_user(3)
+	print(user)
 	assert(user is not None)
+
+def test_get_users():
+	"""
+	Tests we can retrieve all users. For illustrative purposes, client returns mock
+	data. 
+	"""
+	c = Client(mock=True)
+	users = c.get_users()
+	print(users)
+
+	assert(len(users) == 5)
+
+def test_get_posts():
+	"""
+	Tests we can retrieve posts for a user. For illustrative purposes, client returns mock
+	data. 
+	"""
+	c = Client(mock=True)
+	posts = c.get_user_posts(userId=4)
+	print(posts)
+
+	assert(len(posts) == 2)
+
+def test_get_events():
+	"""
+	Tests we can retrieve events related to a user. For illustrative purposes, client returns mock
+	data. 
+	"""
+	c = Client(mock=True)
+	events = c.get_user_events(userId=5, role="hosting")
+	print(events)
+
+	assert(len(events) == 2)
+	assert(len(c.get_user_events(userId=1, role="hosting")) == 0)
