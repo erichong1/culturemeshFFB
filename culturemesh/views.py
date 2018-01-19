8326364562afc4f3e38703d45c68827dccbd8533
Alan <alanf94@stanford.edu>
2018-01-19 15:05:59 -0800

    removed example API call

--- culturemesh/views.py
+++ culturemesh/views.py
@@ -6,17 +6,6 @@ import hashlib
 import http.client as httplib
 import requests
 
-@app.route("/example_api_call")
-def example_api_call():
-	"""
-	For illustrative purposes only.
-	"""
-
-	c = Client()
-	dracula_ebook_num = 345
-	dracula_text = c.get_gutenberg_novel(dracula_ebook_num)
-	return render_template('example.html', example=dracula_text)
-
 @app.route("/")
 def home():
 	return render_template('index.html')
