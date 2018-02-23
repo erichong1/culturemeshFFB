8a1568646352c78c87d9503960ef371c60f1199c
Kelsey Wang <kyw@stanford.edu>
2018-02-23 15:08:49 -0800

    styling changes

--- culturemesh/views.py
+++ culturemesh/views.py
@@ -49,8 +49,14 @@ def login():
 
 @app.route("/search", methods=['GET', 'POST'])
 def render_search_page():
-	form = SearchForm()
-	return render_template('search.html', form=form)
+	if request.method == 'POST':
+		c = Client(mock=True)
+		data = request.form
+		networks = c.get_networks(10, max_id=None) # filter_=data)
+		return render_template('search_results.html', networks=networks)
+	else:
+		form = SearchForm()
+		return render_template('search.html', form=form)
 
 @app.route("/home")
 @app.route("/home/dashboard")
@@ -63,21 +69,6 @@ def render_user_home():
 		return page_not_found("")
 
 	return render_template('home_dashboard.html', user=user)
-
-@app.route("/post")
-def render_post():
-	fake_post = {
-	    "user_id": 3,
-	    "post_text": "Minus cumque corrupti porro natus tenetur delectus illum. Amet aut molestias eaque autem ea odio.\nAsperiores sed officia. Similique accusantium facilis sed. Eligendi tempora nisi sint tempora incidunt perferendis.",
-	    "network_id": 1,
-	    "img_link": "https://www.lorempixel.com/556/586",
-	    "vid_link": "https://dummyimage.com/909x765",
-	    "post_date": "2017-02-01 05:49:35",
-	    "post_class": 0,
-	    "id": 2,
-	    "post_original": "Not sure what this field is"
-	  }
-	return render_template('post.html', post=fake_post)
 
 @app.route("/home/account")
 def render_user_home_account():
@@ -118,8 +109,28 @@ def render_user_home_networks():
 	# TODO: incorporate paging into the user networks call.
 	user_networks = c.get_user_networks(user_id, count=5)
 	# TODO: construct network titles
+
+	titles = []
+	for network in user_networks:
+		title_template = "From %s, %s, %s in %s, %s, %s, that speak %s."
+		location_cur = network['location_cur']
+		city = c.get_city(location_cur['city_id'])['name']
+		region = c.get_region(location_cur['region_id'])['name']
+		country = c.get_country(location_cur['country_id'])['name']
+
+		location_origin = network['location_origin']
+		city_orig = c.get_city(location_origin['city_id'])['name']
+		region_orig = c.get_region(location_origin['region_id'])['name']
+		country_orig = c.get_country(location_origin['country_id'])['name']
+
+
+
+		language = network['language_origin']['name']
+		titles.append(title_template % (city_orig.title(), region_orig.title(), country_orig.title(),
+															      city.title(), region.title(), country.title(), language))
+
 	return render_template('home_networks.html', user=user,
-		user_networks=user_networks)
+		user_network_titles=titles)
 
 ##################### Error handling #########################
 
