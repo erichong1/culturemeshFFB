663c7159b834f187d512748854051f2c58504a73
Lyne <lynetcha1@gmail.com>
2018-04-13 18:56:19 -0700

    Added Mock account verification at login

--- culturemesh/forms.py
+++ culturemesh/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 class LoginForm(FlaskForm):
-    user_id = StringField('User ID', validators=[])
+    email_or_username = StringField('Email/Username', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
