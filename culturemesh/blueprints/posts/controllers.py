5637406a1d37748ec23608ebefa947c54c8a746f
Alan <alanf94@stanford.edu>
2018-05-30 22:11:09 -0700

    post page now fetches posts from the test database

--- culturemesh/blueprints/posts/controllers.py
+++ culturemesh/blueprints/posts/controllers.py
@@ -5,27 +5,38 @@ from flask_login import current_user
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
+POST_TITLE_MAX_LEN = 10
+
 @posts.route("/")
 @flask_login.login_required
 def render_post():
+
   current_post_id = request.args.get('id')
   user_id = current_user.get_id()
-  c = Client(mock=True)
-  fake_post = c.get_post(current_post_id)
-  post_text_arr = fake_post["post_text"].split(" ")
-  title = ""
-  for i in range(10):
-      title += post_text_arr[i] + " "
-  title += "..."
+
+  c = Client(mock=False)
+
+  post = c.get_post(current_post_id)
+  post_text_arr = post["post_text"].split(" ")
+
+  # Currently, the title of a post is simply the first few words
+  title = ' '.join(post_text_arr[:min(len(post_text_arr), POST_TITLE_MAX_LEN)])
+
+  if len(post_text_arr) > POST_TITLE_MAX_LEN:
+    title += "..."
+
+  # NOTE: this only fetches a single post reply
   num_replies = 1
-  replies = c.get_post_replies(fake_post["id"], num_replies)
+  replies = c.get_post_replies(post["id"], num_replies)
+
   usernames = []
   for reply in replies:
       username = c.get_user(reply["id_user"])["username"]
       usernames.append(username)
+
   return render_template('post.html', title=title,
-    post=fake_post, replies=replies,
-    reply_usernames=usernames, num_replies=num_replies)
+    post=post, replies=replies,
+    reply_usernames=usernames, num_replies=len(replies))
 
 @posts.route("/ping")
 @flask_login.login_required
