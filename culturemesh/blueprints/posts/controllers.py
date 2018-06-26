03241d5c817b3f60856798593dcb45a66d120781
Alan <alanf94@stanford.edu>
2018-06-25 22:09:22 -0700

    updates to post page

--- culturemesh/blueprints/posts/controllers.py
+++ culturemesh/blueprints/posts/controllers.py
@@ -1,11 +1,16 @@
+
 from flask import Blueprint, render_template, request
-from culturemesh.client import Client
-import flask_login
 from flask_login import current_user
+from culturemesh.client import Client
+from culturemesh.utils import get_time_ago
+from culturemesh.utils import get_network_title
+
+import flask_login
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 POST_TITLE_MAX_LEN = 10
+NUM_REPLIES_TO_SHOW = 100
 
 @posts.route("/")
 @flask_login.login_required
@@ -19,24 +24,19 @@ def render_post():
   post = c.get_post(current_post_id)
   post_text_arr = post["post_text"].split(" ")
 
-  # Currently, the title of a post is simply the first few words
-  title = ' '.join(post_text_arr[:min(len(post_text_arr), POST_TITLE_MAX_LEN)])
+  post['network_title'] = get_network_title(c.get_network(post['id_network']))
+  post['username'] = c.get_user(post["id_user"])["username"]
+  post['time_ago'] = get_time_ago(post['post_date'])
 
-  if len(post_text_arr) > POST_TITLE_MAX_LEN:
-    title += "..."
+  # NOTE: this will not show more than the latest 100 replies
+  replies = c.get_post_replies(post["id"], NUM_REPLIES_TO_SHOW)
 
-  # NOTE: this only fetches a single post reply
-  num_replies = 1
-  replies = c.get_post_replies(post["id"], num_replies)
-
-  usernames = []
   for reply in replies:
-      username = c.get_user(reply["id_user"])["username"]
-      usernames.append(username)
+      reply['username'] = c.get_user(reply["id_user"])["username"]
+      reply['time_ago'] = get_time_ago(reply['reply_date'])
 
-  return render_template('post.html', title=title,
-    post=post, replies=replies,
-    reply_usernames=usernames, num_replies=len(replies))
+  return render_template('post.html', post=post,
+    replies=replies, num_replies=len(replies))
 
 @posts.route("/ping")
 @flask_login.login_required
