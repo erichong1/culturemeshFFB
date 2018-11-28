af45bfd1595a4f9eb3f93abba0eb13833918be5e
Raymond Yao <raymondyao@DNa1d455d.SUNet>
2018-11-28 00:10:26 -0800

    indexPageUICommit

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export CULTUREMESH_API_KEY=2YoMFGGgKlmzrOd3qCSXiSicCvgEz0Jo
-export WTF_CSRF_SECRET_KEY=Pblk0fdsijSH2AVMu0eu1YiTIl74ItYm
-export CULTUREMESH_API_BASE_ENDPOINT=https://www.culturemesh.com/api-dev/v1
-
-python -u run.py
